344af74782665114562fcd0edb1cf67ffeeb78f9
sarah-duncan <sarahduncan44@gmail.com>
2020-12-22 20:03:04 +0000

    Add activity paths Add activity layout linked by buttons Add Randomise prototype

--- app/src/main/java/com/dunca/charactergenerator/MainActivity.java
+++ app/src/main/java/com/dunca/charactergenerator/MainActivity.java
@@ -1,12 +1,15 @@
 package com.dunca.charactergenerator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mainactivity.R;
+
 
 public class MainActivity extends AppCompatActivity {
     //Defined views
@@ -22,14 +25,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         quickCreateButton = (Button) findViewById(R.id.quick_create_button);
-
         quickCreateButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, QuickCreateActivity.class);
+                Intent intent = new Intent(MainActivity.this, FantasyVRealistic.class);
+                // IS_THIS_QUICK_CREATE determines where the buttons in FantasyVRealistic point to
+                // true= Quick Create
+                // false= Custom Create
+                intent.putExtra("IS_THIS_QUICK_CREATE", true);
                 startActivity(intent);
             }
-                                             }
+        }
         );
+        customCreateButton =(Button) findViewById(R.id.custom_create_button);
+        customCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FantasyVRealistic.class);
+                intent.putExtra("IS_THIS_QUICK_CREATE", false);
+            startActivity(intent);
+        }
+        });
     }
 }
