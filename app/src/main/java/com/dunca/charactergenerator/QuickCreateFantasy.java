7f240706f93974a2cf26662ef0d8668c3cd614a9
sarah-duncan <sarahduncan44@gmail.com>
2021-01-11 18:01:31 +0000

    Change editText to textView in both QuickCreates

--- app/src/main/java/com/dunca/charactergenerator/QuickCreateFantasy.java
+++ app/src/main/java/com/dunca/charactergenerator/QuickCreateFantasy.java
@@ -3,7 +3,7 @@ package com.dunca.charactergenerator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +11,7 @@ import com.example.MainActivity.R;
 
 public class QuickCreateFantasy extends AppCompatActivity {
 
-    private EditText editTextShowDescription;
+    private TextView textViewShowDescription;
     private Button randomiseButton;
     private String[] traits;
 
@@ -21,15 +21,15 @@ public class QuickCreateFantasy extends AppCompatActivity {
         setContentView(R.layout.activity_quick_create_fantasy);
 
         randomiseButton = (Button) findViewById(R.id.randomise_button);
-        editTextShowDescription = (EditText) findViewById(R.id.editText_show_description_fantasy);
+        textViewShowDescription = (TextView) findViewById(R.id.textView_randomise_fantasy);
 
         randomiseButton.setOnClickListener(new View.OnClickListener() {
                                                @Override
                                                public void onClick(View v) {
                                                    Randomise random = new RandomiseFantasy();
                                                    traits= random.randomCharacter();
-                                                   editTextShowDescription.setText("A " + traits[0] + traits[1]+
-                                                           " with "+ traits[2]+ "eyes\n and"+ traits[3] +" hair, who enjoys "+ traits[4]+ ".");
+                                                   textViewShowDescription.setText("A " + traits[0] + " " + traits[1]+
+                                                           " with "+ traits[2]+ " eyes and "+ traits[3] +" hair, who enjoys "+ traits[4]+ ".");
                                                }
                                            }
         );
--- app/src/main/java/com/dunca/charactergenerator/QuickCreateRealistic.java
+++ app/src/main/java/com/dunca/charactergenerator/QuickCreateRealistic.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,7 +16,7 @@ import java.sql.SQLException;
 public class QuickCreateRealistic extends AppCompatActivity implements View.OnClickListener {
 
     //Defined views
-    private EditText editTextShowDescription;
+    private TextView textViewShowDescription;
     private Button randomiseButton;
     private Button addTodoBtn;
     private EditText professionEditText;
@@ -31,7 +32,7 @@ public class QuickCreateRealistic extends AppCompatActivity implements View.OnCl
         setContentView(R.layout.activity_quick_create_realistic);
 
         randomiseButton = (Button) findViewById(R.id.randomise_button);
-        editTextShowDescription = (EditText) findViewById(R.id.editText_show_description_realistic);
+        textViewShowDescription = (TextView) findViewById(R.id.textViewRandomise);
 
         addTodoBtn =(Button) findViewById(R.id.save_button);
 
@@ -48,7 +49,7 @@ public class QuickCreateRealistic extends AppCompatActivity implements View.OnCl
                                                  public void onClick(View v){
                                                      Randomise random = new RandomiseRealistic();
                                                      traits= random.randomCharacter();
-                                                     editTextShowDescription.setText("A " + traits[0] + " with\n "+
+                                                     textViewShowDescription.setText("A " + traits[0] + " with "+
                                                              traits[1]+ " hair, who enjoys "+ traits[2]+ "." );
                                                  }
                                              }
